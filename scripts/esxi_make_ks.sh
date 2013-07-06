57ad3febff0a0c1e486c4b44f88034d04c590f6d
Michal Nowak <michal@casanowak.com>
2013-07-06 12:03:56 +0100

    removed repo option

--- scripts/esxi_make_ks.sh
+++ scripts/esxi_make_ks.sh
@@ -2,14 +2,13 @@
 
 
 if [ $# -lt 5 ]; then
-  echo "Usage: `basename $0` REPO IP NETMASK GATEWAY FQDN"
+  echo "Usage: `basename $0` IP NETMASK GATEWAY FQDN"
   exit 1
 fi
 
-REPO=$1
-IP=$2
-NETMASK=$3
-GATEWAY=$4
-FQDN=$5
+IP=$1
+NETMASK=$2
+GATEWAY=$3
+FQDN=$4
 
-sed "s=@REPO@=${REPO}=g; s=@IP@=${IP}=g; s=@NETMASK@=${NETMASK}=g; s=@GATEWAY@=${GATEWAY}=g; s=@FQDN@=${FQDN}=g;" ../configs/esxi5-ks.template
+sed "s=@IP@=${IP}=g; s=@NETMASK@=${NETMASK}=g; s=@GATEWAY@=${GATEWAY}=g; s=@FQDN@=${FQDN}=g;" ../configs/esxi5-ks.template
